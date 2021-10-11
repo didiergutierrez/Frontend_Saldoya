e1f78cb226348cd1bf0923706e8af4f3f91a4a13
didiergutierrez <digegu0532@gmail.com>
2021-10-11 18:09:37 -0500

    Versión inicial

---
+++ js/Script_prueba.js
@@ -0,0 +1,11 @@
+function obtenerDatos() {
+    var user=document.getElementById("user").value;
+    var pass=document.getElementById("pass").value;
+
+    window.location="index.html"
+
+
+    alert("Sus datos de inicio de seccion son los siguientes: \nUsuario "+user+"\nContraseña: "+pass);
+    alert("Se ha iniciado sesion.")
+
+}
