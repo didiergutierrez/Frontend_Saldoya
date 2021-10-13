25d808fd3e690623ea8e6e570fc1885338deaae9
didiergutierrez <digegu0532@gmail.com>
2021-10-13 16:30:34 -0500

    Cambios a usuario

--- js/Script_prueba.js
+++ js/Script_prueba.js
@@ -1,8 +1,20 @@
 function obtenerDatos() {
     var user=document.getElementById("user").value;
     var pass=document.getElementById("pass").value;
-
-    window.location="index.html"
+    var seleccion=document.getElementById("seleccion").value;
+    if (parseInt(seleccion,10)==1){
+        window.location="perfil_Usuario.html";
+    }
+    else if(parseInt(seleccion,10)==2){
+        window.location="perfil_Administrador.html";
+        a=window.document.getElementById("iniciarSesion");
+        b=window.document.getElementById("registrarse");
+    }
+    else{
+        alert("No has seleccionado rol");
+        window.location="Login.html";
+    }
+    
 
 
     alert("Sus datos de inicio de seccion son los siguientes: \nUsuario "+user+"\nContraseña: "+pass);
